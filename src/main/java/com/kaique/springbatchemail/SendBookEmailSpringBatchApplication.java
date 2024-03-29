976bc317da9c863b4f54e76b57513228751f2d1b
kaiquesilvadev <j.kaique1570@gmail.com>
2024-03-29 19:15:48 -0300

    first commit

---
+++ src/main/java/com/kaique/springbatchemail/SendBookEmailSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.kaique.springbatchemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendBookEmailSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendBookEmailSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaique/springbatchemail/SendBookEmailSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaique.springbatchemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendBookEmailSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
